25a3fe2f6ddb5baf09f6aefa19947c214d33edcb
gorav-code <30561844+gorav-code@users.noreply.github.com>
2022-12-29 05:59:13 +0530

    Formik: Custom controls

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -8,6 +8,10 @@ import { Activity } from "../../../models/activity";
 import { v4 as uuid} from 'uuid';
 import { ErrorMessage, Field, Formik } from "formik";
 import * as Yup from 'yup';
+import MyTextInput from "../../../app/common/form/MyTextInput";
+import MyTextArea from "../../../app/common/form/MyTextArea";
+import MySelectList from "../../../app/common/form/MySelectList";
+import { categoryOptions } from "../../../app/common/form/options/categoryOptions";
 
 export default observer (function ActivityForm(){
     const {activityStore} = useStore();
@@ -29,7 +33,12 @@ export default observer (function ActivityForm(){
 
     //we are using this validationSchema from Yup to apply validations for Formik form
     const validationSchema = Yup.object({
-        title: Yup.string().required('The activity title is required.')
+        title: Yup.string().required('The activity title is required.'),
+        description: Yup.string().required('The activity description is required.'),
+        category: Yup.string().required(), //if we do not want to give custom error msg then we leave it empty and it will show default msg.
+        date: Yup.string().required(),
+        city: Yup.string().required(),
+        venue: Yup.string().required(),
     })
     
     //here we are assigning default state when activity is null. We are passing object with empty values
@@ -92,18 +101,18 @@ export default observer (function ActivityForm(){
                 initialValues={activity} 
                 onSubmit={values => console.log(values)}> 
                 {({handleSubmit}) => (                    
-                     <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
-                        <FormField>
-                            <FormField placeholder='Title' name='title'/>
-                            <ErrorMessage name='title' 
-                                render={error => <Label basic color='red' content={error}/>}/>
-                        </FormField>
-                         <Field placeholder ="Title"  name='title'/>
-                         <Field placeholder ="Description"  name='description'/>
-                         <Field placeholder ="Category"  name='category'/>
-                         <Field placeholder ="Date" type='date'  name='date'/>
-                         <Field placeholder ="City"  name='city'/>
-                         <Field placeholder ="Venue"  name='venue'/>
+                     <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>                        
+                         
+                         {/* using default formik 'Field' */}
+                         {/* <Field placeholder ="Title"  name='title'/> */}
+
+                         {/* using default formik 'Field' */}
+                         <MyTextInput name="title" placeholder="Title"/>
+                         <MyTextArea placeholder="Description" name='description' rows={3}/>
+                         <MySelectList placeholder="Category" name='category' options={categoryOptions} />
+                         <Field  placeholder ="Date" type='date'  name='date'/>
+                         <MyTextInput placeholder ="City"  name='city'/>
+                         <MyTextInput placeholder ="Venue"  name='venue'/>
                          <Button loading={loading} floated='right' positive type='submit' content='Submit'/>
                          <Button as={Link} to='/activities' floated='right' type='button' content='Cancel'/>
                      </Form>
